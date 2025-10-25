6ae3c72c5f7472bd3a1dac904ffe3f205685221a
Abhi2009CU <146779945+Abhi2009CU@users.noreply.github.com>
2025-10-25 11:46:29 -0700

    invert all motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBaseTestAbhinav.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBaseTestAbhinav.java
@@ -5,22 +5,25 @@ import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name="DRIVE_BASE_TEST", group="Linear OpMode")
-public class DriveBaseTest extends LinearOpMode {
+@TeleOp(name="DRIVE_BASE_TEST_ABHINAV", group="Linear OpMode")
+public class DriveBaseTestAbhinav extends LinearOpMode {
 
     private MecanumDrive drive;
 
     @Override
     public void runOpMode() {
         // --- Initialize 4 drive motors ---
-        Motor frontLeft = new Motor(hardwareMap, "frontLeft");
-        Motor frontRight = new Motor(hardwareMap, "frontRight");
-        Motor backLeft = new Motor(hardwareMap, "backLeft");
-        Motor backRight = new Motor(hardwareMap, "backRight");
+        Motor frontLeft = new Motor(hardwareMap, "FrontLeft");
+        Motor frontRight = new Motor(hardwareMap, "FrontRight");
+        Motor backLeft = new Motor(hardwareMap, "BackLeft");
+        Motor backRight = new Motor(hardwareMap, "BackRight");
 
         // Reverse one side for correct drive direction
         frontRight.setInverted(true);
+        frontLeft.setInverted(true);
         backRight.setInverted(true);
+        backLeft.setInverted(true);
+
 
         drive = new MecanumDrive(frontLeft, frontRight, backLeft, backRight);
 
