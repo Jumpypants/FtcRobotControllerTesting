ba61d832d498db54a3eea4ce373c47813d545659
Abhi2009CU <146779945+Abhi2009CU@users.noreply.github.com>
2025-10-25 12:45:12 -0700

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBaseTestAbhinav.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBaseTestAbhinav.java
@@ -12,18 +12,21 @@ public class DriveBaseTestAbhinav extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        // --- Initialize 4 drive motors ---
         Motor frontLeft = new Motor(hardwareMap, "FrontLeft");
         Motor frontRight = new Motor(hardwareMap, "FrontRight");
         Motor backLeft = new Motor(hardwareMap, "BackLeft");
         Motor backRight = new Motor(hardwareMap, "BackRight");
 
-        // Reverse one side for correct drive direction
-        frontRight.setInverted(true);
-        frontLeft.setInverted(true);
-        backRight.setInverted(true);
-        backLeft.setInverted(true);
+        frontLeft.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
 
+        // ✅ Typical mecanum configuration:
+        frontLeft.setInverted(true);
+        frontRight.setInverted(true);
+        backLeft.setInverted(true);
+        backRight.setInverted(true);
 
         drive = new MecanumDrive(frontLeft, frontRight, backLeft, backRight);
 
@@ -31,11 +34,10 @@ public class DriveBaseTestAbhinav extends LinearOpMode {
 
         while (opModeIsActive()) {
             drive.driveRobotCentric(
-                    -gamepad1.left_stick_y,
-                    gamepad1.left_stick_x,
-                    gamepad1.right_stick_x
+                    gamepad1.left_stick_x,   // strafe left/right
+                    -gamepad1.left_stick_y,   // forward/back
+                    gamepad1.right_stick_x    // rotation
             );
-
         }
     }
 }
