787333d6eb1a803632c0e82bd945e6ca845a00c7
Abhi2009CU <146779945+Abhi2009CU@users.noreply.github.com>
2025-11-15 16:23:07 -0800

    change to allow FieldCentric

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBaseWithIntakeTestAbhinav.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBaseWithIntakeTestAbhinav.java
@@ -1,16 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 
 @TeleOp(name="Drive_Base_With_Intake_Test_Abhinav", group="Linear OpMode")
 public class DriveBaseWithIntakeTestAbhinav extends LinearOpMode {
 
     private MecanumDrive drive;
     private CRServo sparkArm;
+    private BNO055IMU imu;
+    private double headingOffset = 0.0;
 
     @Override
     public void runOpMode() {
@@ -27,28 +31,69 @@ public class DriveBaseWithIntakeTestAbhinav extends LinearOpMode {
         backLeft.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
 
-        // ✅ Typical mecanum configuration:
+
         frontLeft.setInverted(true);
         frontRight.setInverted(true);
-        backLeft.setInverted(true);
+        backLeft.setInverted(false);
         backRight.setInverted(true);
 
         drive = new MecanumDrive(frontLeft, frontRight, backLeft, backRight);
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        telemetry.addLine("IMU Initialized. Press Start.");
+        telemetry.update();
 
         waitForStart();
 
         double power;
 
+        headingOffset = imu.getAngularOrientation().firstAngle;
+
+        double headingDeg ,rawHeading;
+
         while (opModeIsActive()) {
-            drive.driveRobotCentric(
-                    gamepad1.left_stick_x,   // strafe left/right
-                    -gamepad1.left_stick_y,   // forward/back
-                    gamepad1.right_stick_x    // rotation
+
+            rawHeading = imu.getAngularOrientation().firstAngle;
+            headingDeg = rawHeading - headingOffset;
+
+            headingDeg = ((headingDeg + 180) % 360) - 180;
+
+            double headingRad = Math.toRadians(headingDeg);
+
+            drive.driveFieldCentric(
+                    gamepad1.left_stick_x, //strafe
+                    -gamepad1.left_stick_y,     // forward/back
+                    gamepad1.right_stick_x,     // rotate
+                    headingDeg, //must be in degrees
+                    false
             );
 
-            power = gamepad1.right_trigger;
+//            drive.driveRobotCentric(
+//                    gamepad1.left_stick_x,   // strafe left/right
+//                    -gamepad1.left_stick_y,   // forward/back
+//                    gamepad1.right_stick_x    // rotation
+//            );
+
+            power = gamepad1.right_trigger - gamepad1.left_trigger;
 
             sparkArm.setPower(power);
+
+            if (gamepad1.y) {
+                headingOffset = imu.getAngularOrientation().firstAngle; //reset
+            }
+
+            telemetry.addData("Heading (deg)", headingDeg);
+//            telemetry.addData("Heading (rad)", headingRad);
 
             telemetry.addData("SPARKmini Power", power);
             telemetry.update();
